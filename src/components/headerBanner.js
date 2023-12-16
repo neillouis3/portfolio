f31d6b176cbe77b7eb3285778fba5b980f009fe6
neillouis3 <neil03.castillon@gmail.com>
2023-12-16 01:53:12 -0330

    1

--- src/components/headerBanner.js
+++ src/components/headerBanner.js
@@ -1,19 +1,39 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useCallback} from "react";
+import Typed from "react-typed";
 
-function HeaderBanner() {
-    const text = [
+
+function HeaderBanner({ text, delay }) {
+    const pText = [
         "Hi there, I'm Neil Louise",
         "Have you tried dragging the boxes?",
         "Have you tried resizing the boxes?",
         "You can reset the layout on the lower right corner."
     ]
+
+    const [newText, setNewText] = useState("");
+
+
+
+    const shuffle = useCallback(() => {
+        const index = Math.floor(Math.random() * pText.length);
+        setNewText(pText[index]);
+    }, []);
+
+    useEffect(() => {
+
+        const intervalID = setInterval(shuffle, 5000);
+        return () => clearInterval(intervalID);
+    }, [shuffle])
+
     return (
+        
         <>
-            <div class="header-banner">
-            
-            
-                <h1>Hi, I'm Dylan</h1>
-            
+            <div>
+
+
+<Typed strings={ [{newText}]} typeSpeed={100} loop />
+
+                
             </div>
         
         </>
