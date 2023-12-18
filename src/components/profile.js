ae96fb6d4353072d4811ad867dd0079d932eb96e
neillouis3 <neil03.castillon@gmail.com>
2023-12-17 21:12:31 -0330

    Prettier

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,33 +2,35 @@ import React from "react";
 import Typed from "react-typed";
 
 function Profile() {
-    return (
-        <>
-            <div className="profileContainer">
-                <div className="profileImageContainer">
-                    <img className="profileImage" src="https://i.imgur.com/4LQ0Jtj.jpg" alt="profile"/>
-                </div>
-                <div className="profileTextContainer">
-                    <h1 className="profileText">Neil Louise</h1>
+	return (
+		<>
+			<div className="profileContainer">
+				<div className="profileImageContainer">
+					<img
+						className="profileImage"
+						src="https://i.imgur.com/4LQ0Jtj.jpg"
+						alt="profile"
+					/>
+				</div>
+				<div className="profileTextContainer">
+					<h1 className="profileText">Neil Louise</h1>
 
-                    <h2 className="profileText"></h2>
-                    <Typed
-                        className="bannerText"
-                        strings={[
-                        "Software Engineer",
-                        "UI/UX Designer",
-                        "Graphic Designer",
-                        ]}
-                        typeSpeed={75}
-                        delaySpeed={1000}
-                        loop
-                    />
-                    
-                </div>
-            </div>
-        
-        </>
-    )
+					<h2 className="profileText"></h2>
+					<Typed
+						className="bannerText"
+						strings={[
+							"Software Engineer",
+							"UI/UX Designer",
+							"Graphic Designer",
+						]}
+						typeSpeed={75}
+						delaySpeed={1000}
+						loop
+					/>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Profile;
