b9d44c3f5ba96dab5c508f40f4e21f13a2f756e2
neillouis3 <neil03.castillon@gmail.com>
2023-12-17 21:11:25 -0330

    Backbone

--- src/components/headerBanner.js
+++ src/components/headerBanner.js
@@ -1,46 +1,33 @@
-import React, {useState, useEffect, useCallback} from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Typed from "react-typed";
 import "./headerBanner.css";
-import { motion } from "framer-motion"
-import Wave from 'react-wavify'
-
-
-
+import { motion } from "framer-motion";
+import Wave from "react-wavify";
 
 function HeaderBanner() {
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
-    return (
+  const windowWidth = window.innerWidth;
+  const windowHeight = window.innerHeight;
+  return (
+    <div className="bannerContainer">
+      <div className="waveContainer"></div>
 
-            <div className="bannerContainer">
-
-                
-                <div className="waveContainer">
-                
-                
-                </div>
-                
-                
-                <div className="textContainer">
-                <h1>
-                <Typed 
-                    className="bannerText"
-                    strings={ [
-                        "Hi there, I'm Neil Louise",
-                        "Have you tried dragging the boxes?",
-                        "Have you tried resizing the boxes?",
-                        "You can reset the layout on the lower right corner."
-                    ]} 
-                    typeSpeed={40} loop />
-
-                </h1>
-                </div>
-                
-                
-            </div>
-        
-
-    )
+      <div className="textContainer">
+        <h1>
+          <Typed
+            className="bannerText"
+            strings={[
+              "Hi there, I'm Neil Louise",
+              "Have you tried dragging the boxes?",
+              "Have you tried resizing the boxes?",
+              "You can reset the layout on the lower right corner.",
+            ]}
+            typeSpeed={40}
+            loop
+          />
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default HeaderBanner;
