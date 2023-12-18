146de09eac1e426f461bc21525bd067a278e5f62
neillouis3 <neil03.castillon@gmail.com>
2023-12-18 02:14:13 -0330

    Progress

---
+++ src/components/social.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Social() {
+
+    return (
+        <>
+        <div className="socialContainer">
+            
+            <a href="https://www.linkedin.com/in/alexander-lee-1b1b1b1b1/" target="_blank" rel="noopener noreferrer">
+            </a>
+        </div>
+
+        
+        </>
+)
+}
