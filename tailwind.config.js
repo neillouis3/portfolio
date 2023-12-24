a2afb8367b5045adb01c2014bfaa9475c2ea21e1
neillouis3 <neil03.castillon@gmail.com>
2023-12-24 10:18:20 -0330

    Tailwind Intialization

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,11 @@ module.exports = {
       }
     }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
 
