e942860470e48c5de395fee8521a54503766f74b
neillouis3 <neil03.castillon@gmail.com>
2023-12-29 21:59:40 -0330

    Components' git push '

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,71 +1,82 @@
 import React from "react";
+import  ComponentSize from "../contants/componentSize.js";
+
 export default function NavBar() {
     return (
         <>
-            <div
+            <nav
                 class="
-                    container
+                    rounded-lg
+                    border-2
+                    border-gray
+                    p-2
+                    
                 "
             >
-                <nav
-                    class="
-                    
-                    "
+                <div
+                    class={`
+                        w-[${ComponentSize.LargeComponent}]
+
+                        bg-transparent
+                        rounder-lg
+                    `}
+                
                 >
                     <ul
                         class="
-                        
+                            flex
+                            flex-row
+
+                            bg-transparent
+                            rounder-lg
+                            gap-4
+                            
                         "
                     >
-                        <li
-                            class="
-                    
-                            "
-                        >
-                            <a href="/">Projects</a>
+                        <li>
+                            <a 
+                                href="/"
+                                class="
+                                    block
+
+                                "
+                            
+                            >Projects</a>
                         </li>
-                        <li
-                            class="
-                        
-                            "
-                        >
-                            <a href="/">Experience</a>
+                        <li>
+                            <a 
+                                href="/"
+                                class="
+                                    block
+
+                                "
+                            >Experience</a>
                         </li>
-                        <li
-                            class="
-                        
-                            "
-                        >
-                            <a href="/">Education</a>
+                        <li>
+                            <a 
+                                href="/"
+                                class="
+                                    block
+
+                                "
+                            >Education</a>
                         </li>
-                        <li
-                            class="
-                    
-                            "
-                        >
-                            <a href="/">Contact</a>
+                        <li>
+                            <a 
+                                href="/"
+                                class="
+                                    block
+                                
+                                "
+                            >Contact</a>
                         </li>
 
 
                     </ul>
+                </div>
+                
+            </nav>
 
-
-
-                    
-
-                    
-
-                    
-
-                    
-                </nav>
-
-
-
-
-            </div>
-        
-        
         </>    
     )
 }
---
+++ src/contants/componentSize.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+const ComponentSize = {
+    SmallComponent: 0.25*window.innerWidth,
+    LargeComponent: 0.50*window.innerWidth,
+}
+
+export default ComponentSize;
+
+
+
+
