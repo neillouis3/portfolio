a0edc8e434769619b46f5b2ac48fc037fc3041bc
neillouis3 <neil03.castillon@gmail.com>
2024-05-04 12:37:39 -0230

    Refactor

---
+++ src/components/experienceBody.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function ExperienceBody() {
+    return (
+        <>
+            <div 
+                class="
+                    container
+                
+                "
+            >
+
+
+
+
+            </div>
+        
+        
+        
+        
+
+
+
+
+        
+        </>
+    );
+
+
+}
