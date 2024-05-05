1557ded35c8918d02ebc1028ff683a040a4559fb
neillouis3 <neil03.castillon@gmail.com>
2024-05-05 19:10:05 -0230

    Initialization

---
+++ src/components/certification.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default function Certification() {
+    return (
+        <>
+            <div 
+                class="
+                    container
+                    flex
+                "
+            >
+                <div
+                    class="
+                        container
+                    "
+                >
+
+                </div>
+                <div
+                    class="
+                        container
+                    "
+                >
+                    <div 
+                        class="
+                            container
+                        "
+                    >
+                        <h1
+                            class="
+                                text-4xl
+                            "
+                        >React JS</h1>
+                    </div>
+                    <div
+                        class="
+                            container
+                        "
+                    >
+                        <h1
+                            class="
+                                text-4xl
+                            "
+                        >Onshape Fundamentals: CAD</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/components/educationBody.js
+++ src/components/educationBody.js
@@ -3,21 +3,14 @@ import React from 'react';
 export default function EducationBody() {
     return (
         <>
+            <div
+                class="
+                    container
+                "
+            >
+                
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+            </div>
         </>
     );
 }
