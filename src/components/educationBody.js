1761c5a1cd99469e7f46f17734e2902b0c1565dc
neillouis3 <neil03.castillon@gmail.com>
2024-05-07 08:09:44 -0230

    Completion

--- src/components/educationBody.js
+++ src/components/educationBody.js
@@ -8,8 +8,58 @@ export default function EducationBody() {
                     container
                 "
             >
-                
+                <div
+                    class="
+                        titleContainer
+                        container
+                    "
+                >
+                    <h1>Education</h1>
+                </div>
+                <div
+                    class="
+                        bodyContainer
+                        container
+                    "
+                >
+                    <div
+                        class="
+                           container 
+                        "
+                    >
 
+                    </div>
+                    <div
+                        class="
+                            container
+                        "
+                    >
+                        <div
+                            class="
+                                universityContainer
+                                container
+                            "
+                        >
+                            <p>SEP 2021 - APR 2026</p>
+                            <p>Computer Engineering</p>
+                            <p>Memorial University of Newfoundland</p>
+                            <p>Grade: 3.9</p>
+                            <p>Transcript:</p>
+                        </div>
+                        <div
+                            class="
+                                highSchoolContainer
+                                container
+                            "
+                        >
+                            <p>SEP 2021 - APR 2026</p>
+                            <p>Highschool Education</p>
+                            <p>Jeddah International School</p>
+                            <p>Grade: 3.9</p>
+                            <p>Transcript:</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     );
