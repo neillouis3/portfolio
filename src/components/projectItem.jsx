0fffbf734a1303236c320475aa729dde93f87a71
Neil Louise <neil03.castillon@gmail.com>
2024-06-06 10:54:57 +0300

    Fixed issues

--- src/components/projectItem.jsx
+++ src/components/projectItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function ProjectItem({title, description, imageLink, githubLink, demoLink}) {
     const  [isHovered, setIsHovered] = useState(false);
