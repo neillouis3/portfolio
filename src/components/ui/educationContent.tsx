51e2626f542d402d9668641f648dbf196edc7a47
Neil Louise <neil03.castillon@gmail.com>
2024-12-06 02:33:52 +0300

    f

--- src/components/ui/educationContent.tsx
+++ src/components/ui/educationContent.tsx
@@ -8,6 +8,10 @@ import {Accordion, AccordionItem, Avatar} from "@nextui-org/react";
 
 export default function EducationContent() {
 
+    const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+
     
     const itemClasses = {
         base: "py-0 w-full",
@@ -19,7 +23,60 @@ export default function EducationContent() {
     return (
         <div className="w-full bg-default-100 h-[60vh] rounded-xl -mt-16 p-8 flex flex-row">
                 
-
+                <Accordion selectionMode="multiple">
+                    <AccordionItem
+                        key="1"
+                        aria-label="Chung Miller"
+                        startContent={
+                        <Avatar
+                            isBordered
+                            color="primary"
+                            radius="lg"
+                            src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
+                        />
+                        }
+                        subtitle="4 unread messages"
+                        title="Chung Miller"
+                    >
+                        {defaultContent}
+                    </AccordionItem>
+                    <AccordionItem
+                        key="2"
+                        aria-label="Janelle Lenard"
+                        startContent={
+                        <Avatar
+                            isBordered
+                            color="success"
+                            radius="lg"
+                            src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                        />
+                        }
+                        subtitle="3 incompleted steps"
+                        title="Janelle Lenard"
+                    >
+                        {defaultContent}
+                    </AccordionItem>
+                    <AccordionItem
+                        key="3"
+                        aria-label="Zoey Lang"
+                        startContent={
+                        <Avatar
+                            isBordered
+                            color="warning"
+                            radius="lg"
+                            src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+                        />
+                        }
+                        subtitle={
+                        <p className="flex">
+                            2 issues to<span className="text-primary ml-1">fix now</span>
+                        </p>
+                        }
+                        title="Zoey Lang"
+                    >
+                        {defaultContent}
+                    </AccordionItem>
+                    </Accordion>
             
         </div>
     )
